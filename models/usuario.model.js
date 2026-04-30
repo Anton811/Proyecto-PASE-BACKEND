a5c85c7d0a2c2d3c33aad80b524bb7d4fd5b3050
Anton811 <eduardoamg811@gmail.com>
2026-04-30 09:42:45 -0600

    Cambio de parametros Main

--- models/usuario.model.js
+++ models/usuario.model.js
@@ -5,16 +5,17 @@ const usuario = {
   buscarRegistro: async (data) => {
     const { email, telefono } = data;
     const [result] = await db.query(
-      "SELECT * from usuario WHERE correo = ? OR telefono = ?",
+      "SELECT * from usuario WHERE correoUsuario = ? OR telUsuario = ?",
       [email, telefono],
     );
 
     return result.length > 0;
   },
   buscarLogin: async (data) => {
-    const [result] = await db.query("SELECT * FROM usuario WHERE correo = ?", [
-      data,
-    ]);
+    const [result] = await db.query(
+      "SELECT * FROM usuario WHERE correoUsuario = ?",
+      [data],
+    );
 
     return result[0];
   },
@@ -33,7 +34,7 @@ const usuario = {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const [result] = await db.query(
-      "INSERT INTO usuario (nombreUsuario, appUsuario, apmUsuario, telefono, correo, idTipoUsuario, fechaDeRegistro, password) VALUES (?, ?, ?, ?, ?, ?, NOW(), ?)",
+      "INSERT INTO usuario (nombreUsuario, appUsuario, apmUsuario, telUsuario, correoUsuario, idTipoUsuario, passwordUsuario) VALUES (?, ?, ?, ?, ?, ?, ?)",
       [
         nombre,
         app,
