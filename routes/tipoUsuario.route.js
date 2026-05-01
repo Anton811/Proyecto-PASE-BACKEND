b9e55945acca33bdeb688c8ca8a9223b1780c3c3
Anton811 <eduardoamg811@gmail.com>
2026-04-30 22:35:56 -0600

    correcion de letra

--- routes/tipoUsuario.route.js
+++ routes/tipoUsuario.route.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const tipoUsuarioControl = require("../controllers/tipoUsuario.controller");
 
-router.get("/buscarTiposUsuario", tipoUsuarioControl.buscarTipoUsuario);
+router.get("/buscarTipoUsuario", tipoUsuarioControl.buscarTipoUsuario);
 
 module.exports = router;
