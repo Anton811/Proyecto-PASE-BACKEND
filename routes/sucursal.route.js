986d1ab4e4f2898851599cbcd6a46d55d3536e0b
Anton811 <eduardoamg811@gmail.com>
2026-05-01 12:14:56 -0600

    Fix router initialization in sucursal routes

--- routes/sucursal.route.js
+++ routes/sucursal.route.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router;
+const router = express.Router();
 const sucursalControl = require("../controllers/sucursal.controller");
 
 router.get("/cargarEstados", sucursalControl.buscarEstado);
